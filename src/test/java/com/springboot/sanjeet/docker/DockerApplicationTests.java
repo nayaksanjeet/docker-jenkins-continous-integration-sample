5c6d1982e85fc7308e3fb9ff09017054827105bf
Sanjeet Kumar Nayak <40313847+nayaksanjeet@users.noreply.github.com>
2023-05-16 22:09:12 +0530

    1st commit
    1st commit

---
+++ src/test/java/com/springboot/sanjeet/docker/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.sanjeet.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
